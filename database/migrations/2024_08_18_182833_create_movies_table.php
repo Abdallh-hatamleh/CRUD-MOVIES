1c572a81c40683f46d2fae7060b5859fc5c66110
Abdallh-hatamleh <abdallh.a.hatamleh@gmail.com>
2024-08-19 11:22:55 +0300

    soft delete added

--- database/migrations/2024_08_18_182833_create_movies_table.php
+++ database/migrations/2024_08_18_182833_create_movies_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('desc');
             $table->string('genre');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
